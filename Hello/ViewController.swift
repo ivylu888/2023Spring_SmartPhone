c29b801600648a380f08eee2511026e138d72679
Ivy Lu <wanchinglu0710@gmail.com>
2023-01-16 17:02:55 -0800

    yeah

--- Hello/ViewController.swift
+++ Hello/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var myLabel1: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func myButton(_ sender: Any) {
+        myLabel1.text = "hello World"
+    }
 }
 
